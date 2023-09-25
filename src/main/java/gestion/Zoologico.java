dc5ecef1ea23275f3ef35b61690da137adb06031
JPosada64 <141966165+mquirozr@users.noreply.github.com>
2023-09-24 21:49:16 -0500

    prueba9

--- src/main/java/gestion/Zoologico.java
+++ src/main/java/gestion/Zoologico.java
@@ -1,6 +1,58 @@
 package gestion;
 
 import java.util.ArrayList;
+
+public class Zoologico {
+	private String nombre;
+	private String ubicacion;
+	private ArrayList<Zona> zonas;
+	
+	public Zoologico() {
+		this.nombre = null;
+		this.ubicacion = null;
+		this.zonas = new ArrayList<Zona>();
+	}
+	
+	public Zoologico(String nombre, String ubicacion) {
+		this.nombre = nombre;
+		this.ubicacion = ubicacion;
+		this.zonas = new ArrayList<Zona>();
+	}
+	
+	public int cantidadTotalAnimales() {
+        int totalAnimales = 0;
+        for (Zona zona : zonas) {
+            totalAnimales += zona.cantidadAnimales();
+        }
+        return totalAnimales;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getUbicacion() {
+		return ubicacion;
+	}
+	public void setUbicacion(String ubicacion) {
+		this.ubicacion = ubicacion;
+	}
+	public ArrayList<Zona> getZona() {
+		return zonas;
+	}
+	public void setZonas(ArrayList<Zona> zonas) {
+		this.zonas = zonas;
+	}
+	
+	public void agregarZonas(Zona zona) {
+		this.zonas.add(zona);
+	}
+}
+/*package gestion;
+
+import java.util.ArrayList;
 import java.util.List;
 import gestion.Zona;
 
@@ -58,4 +110,4 @@ public class Zoologico {
 
 
 
-}
+}*/
